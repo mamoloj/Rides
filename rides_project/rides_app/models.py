c9b71a5b48429e3db7b2b90a5a9ee8ccce9125b5
unknown <mamoloj@gmail.com>
2024-08-05 08:57:15 +0800

    setting up custom user

--- rides_project/rides_app/models.py
+++ rides_project/rides_app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'admin')
+        return self.create_user(email, password, **extra_fields)
+
+
+
+class User(AbstractBaseUser):
+    id_user = models.AutoField(primary_key=True)
+    role = models.CharField(max_length=50, blank=True, null=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    @property
+    def id(self):
+        return self.id_user
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser or super().has_perm(perm, obj)
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser or super().has_module_perms(app_label)
