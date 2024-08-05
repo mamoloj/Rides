8a7cb315b5a7a2f860410e9bed44ef4528e7a839
unknown <mamoloj@gmail.com>
2024-08-05 09:01:46 +0800

    setup serializers for custom users

---
+++ rides_project/rides_project/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        token['role'] = user.role
+        return token
+
+    def validate(self, attrs):
+        credentials = {
+            'email': attrs.get('email'),
+            'password': attrs.get('password')
+        }
+
+        user = User.objects.filter(email=credentials['email']).first()
+        if user and user.check_password(credentials['password']):
+            data = super().validate(attrs)
+            data['email'] = user.email
+            data['role'] = user.role
+            return data
+
+        raise serializers.ValidationError('Invalid credentials')
