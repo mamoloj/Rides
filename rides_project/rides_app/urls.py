fe9942728d394a5f16af499f6e01941e3dbbf14a
unknown <mamoloj@gmail.com>
2024-08-05 11:33:55 +0800

    fixing issue on urls

--- rides_project/rides_app/urls.py
+++ rides_project/rides_app/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 from .views import MyTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, RideEventViewSet
+from .views import RideViewSet, UserViewSet, RideEventViewSet
 
 
 router = DefaultRouter()
+router.register(r'rides', RideViewSet)
 router.register(r'users', UserViewSet)
 router.register(r'ride-events', RideEventViewSet)
 
@@ -13,5 +14,5 @@ router.register(r'ride-events', RideEventViewSet)
 urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('', include(router.urls)),
 ]
